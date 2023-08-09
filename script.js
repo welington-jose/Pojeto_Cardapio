311cff809d68d5d1dc5cf7a357f708d51e273d06
welington-jose <114547664+welington-jose@users.noreply.github.com>
2023-08-08 23:17:39 -0400

    PROJETO

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const items = [
+const pasteis = [
     {
         id: 0,
         nome: 'Pastel de carne',
@@ -25,40 +25,107 @@ const items = [
     },
 ]
 
-    inicializarCardapio = ()=>{
+    inicializarProdutos = ()=>{
         var conteinerProdutos = document.getElementById('pasteis');
-        items.forEach((val)=>{
+        pasteis.forEach((val)=>{
             conteinerProdutos.innerHTML +=`
             
             <div class="produto-single">
-            <img src="${val.img}"/>
+            <span class="quantidade">${val.quantidade}</span>
+            <img src="${val.img}" data-key=${val.id} class="adicionar"/>
             <p>${val.nome}</p>
             <p>${val.valor}</p>
-            <a key="${val.id}" href="#">Adiocionar ao pedido</a>
-
+            
             </div>
             `
-            ;
-            
+             
         });
-
     };
+    
+inicializarProdutos();  
 
-inicializarCardapio();
+        var linksPasteis = document.querySelectorAll('.adicionar')
 
-        atualizarPedido = () => {
-            console.log(items);
-        }
-        const links = document.querySelectorAll('a')
-
-        links.forEach((link)=>{
+        linksPasteis.forEach((link)=>{
             link.addEventListener('click',()=>{
-                const key = link.getAttribute('key');
-                items[key].quantidade++;
-                atualizarPedido();
-                return false;
-            
-            
-            });
+                const key = link.getAttribute('data-key');
+                pasteis[key].quantidade++;
+
+                const quantidadeElement = link.parentElement.querySelector('.quantidade');
+                 if (quantidadeElement) {
+                  quantidadeElement.textContent = pasteis[key].quantidade;
+                  quantidadeElement.style.visibility = pasteis[key].quantidade > 0 ? 'visible' : 'hidden';
+}
+        return false;
+                });
         });
+
+
            
+        const bebidas = [
+            {
+                id: 0,
+                nome: 'Coca Cola lata 300ml',
+                img: 'imagens/bebidas/coca_lt300.jpg',
+                quantidade: 0,
+                valor: 'R$ 4,00',
+        
+            },
+            {
+                id: 1,
+                nome: 'Coca cola zero lata 300ml',
+                img: 'imagens/bebidas/coca0_lt300.jpg',
+                quantidade: 0,
+                valor: 'R$ 4,00',
+        
+            },
+            {
+                id: 2,
+                nome: 'Guaraná lata 300ml',
+                img: 'imagens/bebidas/guarana_lt300.jpg',
+                quantidade: 0,
+                valor: 'R$ 4,00',
+        
+            },
+        ]
+        
+            inicializarBebidas = ()=>{
+                var conteinerProdutos = document.getElementById('bebidas');
+                bebidas.forEach((val)=>{
+                    conteinerProdutos.innerHTML +=`
+                    
+                    <div class="produto-single">
+                    <span class="quantidade">${val.quantidade}</span>
+                    <img src="${val.img}" data-key=${val.id} class="adicionar"/>
+                    <p>${val.nome}</p>
+                    <p>${val.valor}</p>
+                    
+                    </div>
+                    `
+                    
+                    
+                });
+        
+            };
+        
+        inicializarBebidas();
+        
+               
+                
+                var linksBebidas = document.querySelectorAll('.adicionar')
+        
+                linksBebidas.forEach((link)=>{
+                    link.addEventListener('click',()=>{
+                        const key = link.getAttribute('data-key');
+                        bebidas[key].quantidade++;
+        
+        
+                        const quantidadeElement = link.parentElement.querySelector('.quantidade');
+                         if (quantidadeElement) {
+                          quantidadeElement.textContent = bebidas[key].quantidade;
+                          quantidadeElement.style.visibility = bebidas[key].quantidade > 0 ? 'visible' : 'hidden';
+        
+                }
+                        return false;
+                    });
+                });
