e9e8b7ab950cc31b3ba8537fee0187d4deb2bbbf
welington-jose <114547664+welington-jose@users.noreply.github.com>
2023-10-30 21:41:05 -0400

    projeto

--- pedido.js
+++ pedido.js
@@ -1,10 +1,8 @@
-
 function adicionarItensPedido() {
-
     const pedido = document.getElementById('pedido');
-    pedido.innerHTML = '';
+    pedido.innerHTML = ''; // Limpar a exibição do pedido
     
-   
+
     for (let i = 0; i < localStorage.length; i++) {
         const chave = localStorage.key(i);
         if (chave.startsWith('quantidade_bebidas_')) {
@@ -12,79 +10,80 @@ function adicionarItensPedido() {
             const quantidade = parseInt(localStorage.getItem(chave));
 
             if (quantidade > 0) {
-
+                // Recuperar detalhes da bebida
                 const nomeBebida = localStorage.getItem(`nome_bebidas_${id}`);
                 const imgBebida = localStorage.getItem(`img_bebidas_${id}`);
-
                 const valorBebida = localStorage.getItem(`valor_bebidas_${id}`);
                 const valorBebidaNumero = parseFloat(valorBebida.replace('R$ ', '').replace(',', '.'));
-
                 const valorTotal = valorBebidaNumero * quantidade;
 
-                const itemPedido = document.createElement('div');
+                const itemPedido = document.createElement('div'); // Criar um novo elemento de item
                 itemPedido.innerHTML = `
-                
-                <div class="pd_bebidas">
-                <p class="img_pd"><img src=${imgBebida} width="50px"></p>
-                <p class="nome_pd">${nomeBebida}</p>
-                <p class="qtd_pd"><input type="number" value="${quantidade}" min="0"></p> 
-                <p class="totais"> Total R$ ${valorTotal.toFixed(2)} </p>
-                </div>  <hr>
-    
-    `;
-                pedido.appendChild(itemPedido);
+                    <div class="pd_bebidas">
+                        <p class="img_pd"><img src=${imgBebida} width="50px"></p>
+                        <p class "nome_pd">${nomeBebida}</p>
+                        <p class="qtd_pd"><input type="number" value="${quantidade}" min="0"></p>
+                        <p class="totais"> Total R$ ${valorTotal.toFixed(2)} </p>
+                    </div>
+                    <hr>
+                `;
+                pedido.appendChild(itemPedido); // Adicionar o item à exibição do pedido
 
-
-                // Adicione um ouvinte de evento ao input para atualizar o localStorage
+                // Adicionar um ouvinte de evento ao campo de entrada de quantidade
                 const inputQuantidade = itemPedido.querySelector('input[type="number"]');
                 inputQuantidade.addEventListener('input', () => {
-                    const newQuantity = parseInt(inputQuantidade.value);
-                    localStorage.setItem(chave, newQuantity); // Atualiza a quantidade no localStorage
-                    somar(); // Recria a lista de itens no pedido
+                    const novaQuantidade = parseInt(inputQuantidade.value);
+                    if (novaQuantidade <= 0) {
+                        localStorage.removeItem(chave);
+                        itemPedido.style.display = 'none'; // Ocultar o item no pedido
+                    }
+                    localStorage.setItem(chave, novaQuantidade); // Atualizar a quantidade no armazenamento local
+                    somarPedido(); // Recriar a lista de itens no pedido
                 });
             }
-            
-        };
+        }
 
         if (chave.startsWith('quantidade_pasteis_')) {
             const id = chave.replace('quantidade_pasteis_', '');
             const quantidade = parseInt(localStorage.getItem(chave));
 
             if (quantidade > 0) {
-
+                // Recuperar detalhes do pastel
                 const nomePasteis = localStorage.getItem(`nome_pasteis_${id}`);
                 const imgPasteis = localStorage.getItem(`img_pasteis_${id}`);
-
                 const valorPasteis = localStorage.getItem(`valor_pasteis_${id}`);
                 const valorPasteisNumero = parseFloat(valorPasteis.replace('R$ ', '').replace(',', '.'));
-
                 const valorTotal = valorPasteisNumero * quantidade;
 
-                const itemPedido = document.createElement('div');
+                const itemPedido = document.createElement('div'); // Criar um novo elemento de item
                 itemPedido.innerHTML = `
-                
-        <div class="pd_pasteis">
-        <p class="img_pd"><img src=${imgPasteis} width="50px"></p>
-        <p class="nome_pd">${nomePasteis}</p>
-        <p class="qtd_pd"><input type="number" value="${quantidade}" min="0"></p> 
-        <p class="totais"> Total R$ ${valorTotal.toFixed(2)} </p>
-        </div>  <hr>
+                    <div class="pd_pasteis">
+                        <p class="img_pd"><img src=${imgPasteis} width="50px"></p>
+                        <p class="nome_pd">${nomePasteis}</p>
+                        <p class="qtd_pd"><input type="number" value="${quantidade}" min="0"></p>
+                        <p class="totais"> Total R$ ${valorTotal.toFixed(2)} </p>
+                    </div>
+                    <hr>
+                `;
+                pedido.appendChild(itemPedido); // Adicionar o item à exibição do pedido
 
-    `;
-                pedido.appendChild(itemPedido);
-
-                // Adicione um ouvinte de evento ao input para atualizar o localStorage
+                // Adicionar um ouvinte de evento ao campo de entrada de quantidade
                 const inputQuantidade = itemPedido.querySelector('input[type="number"]');
                 inputQuantidade.addEventListener('input', () => {
-                    const newQuantity = parseInt(inputQuantidade.value);
-                    localStorage.setItem(chave, newQuantity); // Atualiza a quantidade no localStorage
-                    somarPedido(); 
+                    const novaQuantidade = parseInt(inputQuantidade.value);
+                    if (novaQuantidade <= 0) {
+                        localStorage.removeItem(chave);
+                        itemPedido.style.display = 'none'; // Ocultar o item no pedido
+                    }
+                    localStorage.setItem(chave, novaQuantidade); // Atualizar a quantidade no armazenamento local
+                    somarPedido(); // Recriar a lista de itens no pedido
                 });
             }
-            
         }
     }
+    somarPedido(); // Atualizar o valor total do pedido
 }
+
 function somarPedido() {
     let totalPedido = 0;
 
@@ -103,30 +102,32 @@ function somarPedido() {
                 }
             }
         } else if (chave.startsWith('quantidade_pasteis_')) {
-            if (chave.startsWith('quantidade_pasteis_')) {
-                const id = chave.replace('quantidade_pasteis_', '');
-                const quantidade = parseInt(localStorage.getItem(chave));
-                const valorPasteis = localStorage.getItem(`valor_pasteis_${id}`);
-    
-                if (valorPasteis && !isNaN(quantidade)) {
-                    const valorPasteisNumero = parseFloat(valorPasteis.replace('R$ ', '').replace(',', '.'));
-                    if (!isNaN(valorPasteisNumero)) {
-                        totalPedido += quantidade * valorPasteisNumero;
-                    }
+            const id = chave.replace('quantidade_pasteis_', '');
+            const quantidade = parseInt(localStorage.getItem(chave));
+            const valorPasteis = localStorage.getItem(`valor_pasteis_${id}`);
+
+            if (valorPasteis && !isNaN(quantidade)) {
+                const valorPasteisNumero = parseFloat(valorPasteis.replace('R$ ', '').replace(',', '.'));
+                if (!isNaN(valorPasteisNumero)) {
+                    totalPedido += quantidade * valorPasteisNumero;
                 }
             }
         }
     }
 
     const somaTotal = document.getElementById('somaTotal');
-    somaTotal.innerHTML = `R$ ${totalPedido.toFixed(2)}`;
+    if (somaTotal) {
+        somaTotal.innerHTML = `R$ ${totalPedido.toFixed(2)}`;
+        somaTotal.style.display = totalPedido < 1 ? 'none' : 'block';
+
+    }
 }
 
-adicionarItensPedido();
-somarPedido();
+adicionarItensPedido(); // Adicionar itens ao pedido
+somarPedido(); // Calcular o valor total do pedido inicial
 
-// Adicione o ouvinte de eventos para campos de quantidade
+// Adicionar ouvintes de evento aos campos de entrada de quantidade
 const inputsQuantidade = document.querySelectorAll('input[type="number"]');
 inputsQuantidade.forEach((input) => {
-    input.addEventListener('input', somarPedido);
+    input.addEventListener('input', somarPedido); // Recalcular o valor total do pedido quando o input é alterado
 });
